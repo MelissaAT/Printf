75efdbb096fc5719b87ba34f9e5010c4c40c3abf
rvegarodz <r.vega27@outlook.com>
2022-11-11 16:49:10 +0000

    testing functin

--- main.c
+++ main.c
@@ -19,8 +19,8 @@ int main(void)
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
     addr = (void *)0x7ffe637541f0;
-    record = _printf("Length:[%   d, %i]\n", len, len);
-    printf("Return: %d\n", record);
+    _printf("Length:[%   d, %i]\n", len, len);
+    /**printf("Return: %d\n", record);*/
     record = printf("Length:[%d, %i]\n", len2, len2);
     printf("Return: %d\n", record);
     record = _printf("Negative:[%d]\n", -762534);
@@ -49,5 +49,7 @@ int main(void)
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
     printf("Unknown:[%r]\n");
+
+   
     return (0);
 }
